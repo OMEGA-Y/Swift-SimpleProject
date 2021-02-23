2b1b6e31211a170729994d246ef6d576ac1a60ff
OMEGA-Y <science_co@naver.com>
2021-02-23 20:34:38 +0900

    Initial Commit

---
+++ Shared/SimpleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleProjectApp.swift
+//  Shared
+//
+//  Created by YunseonChoi on 2021/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
